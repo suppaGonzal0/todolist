d667feb386e762a83ab7b7a62cca3ed63546385b
Mehzabin Sadat Aothoi <mehzabin.sadat.aothoi@g.bracu.ac.bd>
2022-02-03 13:17:26 +0600

    App & index update

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,12 @@
 import './App.css';
-import Lists from './components/Lists/lists';
 import Tasks from './components/Tasks/tasks';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
 
 function App() {
-
-  const [list, setList] = useState([]);
-  const [listid, setListId] = useState(0);
-
-  useEffect(() => {
-    const getList = () => {
-      axios.get("http://localhost:3001/list").then((response) => {
-      setList(response.data);
-      });
-    };
-    getList();
-  }, [list])
-
   return (
-    <div className="App">
-      <header>Todo-List</header>
-      <Lists list={list} setListId={(listid) => setListId(listid)}/>
-      <Tasks listid={listid}/>
-    </div>
+      <div className="App">
+        <header>TODO LIST</header>
+        <Tasks/>
+      </div>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -6,41 +6,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const port= 3001;
-
 const db = mysql.createPool({
   user: "root",
   host: "localhost",
   password: "1234",
   database: "todolist"
-});
-
-//getlist
-app.get('/list', (req, res) => {
-  db.query("select * from list", (err, result) => {
-    if(err){
-      console.log(err);
-    }
-    else{
-      res.send(result);
-    }
-  });
-});
-
-//addList
-app.post('/list', (req,res) => {
-  const listname = req.body.listname;
-
-  db.query("insert into list (listname) values (?)", [listname], 
-    (err, result) => {
-      if(err){
-        console.log(err);
-      }
-      else{
-        res.send(result);
-      }
-    }
-  );
 });
 
 //getAllTasks
@@ -55,10 +25,9 @@ app.get('/task', (req, res) => {
   });
 });
 
-//getTasksbyId
-app.get('/task/:id', (req, res) => {
-const id = req.params.id;
-  db.query("select * from task where listid = ?", id, (err, result) => {
+//getActive
+app.get('/tasksActive', (req, res) => {
+  db.query("select * from task where status=false", (err, result) => {
     if(err){
       console.log(err);
     }
@@ -68,11 +37,52 @@ const id = req.params.id;
   });
 });
 
+//getCompleted
+app.get('/tasksCompleted', (req, res) => {
+  db.query("select * from task where status=true", (err, result) => {
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+//getTasksLeft
+app.get('/tasksLeft', (req, res) => {
+  db.query("select count(taskname) as tasksLeft from task where status=false", (err, result) => {
+    if(err){
+      console.log(err);
+    }
+    else{
+      res.send(result);
+    }
+  });
+});
+
+//addTask
 app.post('/task', (req,res) => {
   const taskname = req.body.taskname;
-  const listid = req.body.listid;
 
-  db.query("insert into task (taskname, listid) values (?, ?)", [taskname, listid], 
+  db.query("insert into task (taskname) values (?)", [taskname], 
+    (err, result) => {
+      if(err){
+        console.log(err);
+      }
+      else{
+        res.send(result);
+      }
+    }
+  );
+});
+
+//changeStatus
+app.put('/task', (req,res) => {
+
+  const taskid = req.body.taskid;
+
+  db.query("update task set status = if (status, false, true) where taskid=?", [taskid], 
     (err, result) => {
       if(err){
         console.log(err);
